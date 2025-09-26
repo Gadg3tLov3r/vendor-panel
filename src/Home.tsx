caf2d4173fa30cf887e44d2295b638ba3f8dafe1
Gadg3tLov3r <dev2@cmiyc.pro>
2025-09-27 01:58:13 +0600

    Add initial project structure with components, routing, and theme support

---
+++ src/Home.tsx
@@ -0,0 +1,10 @@
+import Layout from "./components/layout";
+import { SectionCards } from "./components/section-cards";
+
+export default function Home() {
+  return (
+    <Layout>
+      <SectionCards />
+    </Layout>
+  );
+}
